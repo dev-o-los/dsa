75d1490188665ee3ec7a39bbc87fed3f4f58f1b5
devolos <utk27032005@gmail.com>
2025-07-18 18:52:42 +0530

    Enhance Graph class with adjacency list representation and methods for adding edges and calculating degrees. Added functionality for both directed and undirected graphs, including methods to display the adjacency list and find node degrees.

--- Graph.java
+++ Graph.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The Graph class in Java represents a graph data structure with methods to add
  * weighted edges and
@@ -7,11 +10,34 @@
 public class Graph {
 
     int adjMat[][];
+    List<List<Integer>> adjList;
 
     Graph(int nodes) {
         adjMat = new int[nodes][nodes];
+        adjList = new ArrayList<>();
+        for (int i = 0; i < nodes; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
     }
 
+    /**
+     * The function `addEdgesInMat` adds edges to an adjacency matrix based on the
+     * input array of edges
+     * and whether the graph is directed or undirected.
+     * 
+     * @param edges      The `edges` parameter is a 2D array where each row
+     *                   represents an edge between two
+     *                   vertices. Each row contains two elements: the source vertex
+     *                   and the destination vertex of the
+     *                   edge.
+     * @param isDirected The `isDirected` parameter in the `addEdgesInMat` method
+     *                   indicates whether the
+     *                   graph is directed or undirected. If `isDirected` is `true`,
+     *                   it means the graph is directed, and
+     *                   edges are added only in one direction from `u` to `v`. If
+     *                   `is
+     */
     public void addEdgesInMat(int edges[][], boolean isDirected) {
         for (int edge[] : edges) {
             int u = edge[0];
@@ -26,6 +52,53 @@ public class Graph {
         }
     }
 
+    /**
+     * The function `addEdgesInList` adds edges to an adjacency list representation
+     * of a graph,
+     * considering whether the graph is directed or undirected.
+     * 
+     * @param edges      The `edges` parameter is a 2D array where each row
+     *                   represents an edge between two
+     *                   vertices. Each row contains two elements: the source vertex
+     *                   and the destination vertex of the
+     *                   edge.
+     * @param isDirected The `isDirected` parameter is a boolean value that
+     *                   indicates whether the graph
+     *                   is directed or undirected. If `isDirected` is true, it
+     *                   means the graph is directed, and if it is
+     *                   false, it means the graph is undirected.
+     */
+    public void addEdgesInList(int edges[][], boolean isDirected) {
+        for (int edge[] : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (isDirected) {
+                adjList.get(u).add(v);
+            } else {
+                adjList.get(u).add(v);
+                adjList.get(v).add(u);
+            }
+        }
+    }
+
+    /**
+     * The function `addWeightedGraph` adds weighted edges to an adjacency matrix
+     * based on the input
+     * edges array and whether the graph is directed or undirected.
+     * 
+     * @param edges      The `edges` parameter is a 2D array where each row
+     *                   represents an edge in the graph.
+     *                   Each row contains three elements: the starting vertex `u`,
+     *                   the ending vertex `v`, and the weight
+     *                   `w` of the edge between `u` and `v`.
+     * @param isDirected The `isDirected` parameter in the `addWeightedGraph` method
+     *                   indicates whether
+     *                   the graph is directed or undirected. If `isDirected` is
+     *                   `true`, it means the graph is directed,
+     *                   and the edges have a specific direction from one vertex to
+     *                   another. If `isDirected` is
+     */
     public void addWieghtedGraph(int edges[][], boolean isDirected) {
         for (int edge[] : edges) {
             int u = edge[0];
@@ -51,15 +124,60 @@ public class Graph {
         }
     }
 
+    public void displayList() {
+        for (int i = 0; i < adjList.size(); i++) {
+            System.out.print(i + "-> ");
+            System.out.print(adjList.get(i));
+
+        }
+    }
+
+    public void findDegreeInUnDirectedGraph(int edges[][], int nodes) {
+        int degree[] = new int[nodes];
+        for (int edge[] : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            degree[u]++;
+            degree[v]++;
+        }
+
+        for (int i = 0; i < nodes; i++) {
+            System.out.println("node => " + i + " degree => " + degree[i]);
+        }
+    }
+
+    public void findDegreeOutUnDirectedGraph(int edges[][], int nodes) {
+        int indegree[] = new int[nodes];
+        int outdegree[] = new int[nodes];
+        for (int edge[] : edges) {
+            int from = edge[0];
+            int to = edge[1];
+
+            indegree[to]++;
+            outdegree[from]++;
+        }
+
+        for (int i = 0; i < nodes; i++) {
+            System.out.print("node => " + i + " in-degree => " + indegree[i]);
+            System.out.println("node => " + i + " out-degree => " + outdegree[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        int edges[][] = { { 0, 2, 10 }, { 0, 1, 20 }, { 1, 3, 30 } };
+        int edges[][] = { { 0, 2 }, { 0, 1 }, { 1, 3 } };
         int nodes = 4;
         Graph graph = new Graph(nodes);
+        graph.findDegreeInUnDirectedGraph(edges, nodes);
+        graph.findDegreeOutUnDirectedGraph(edges, nodes);
+
         // graph.addEdgesInMat(edges, true);
         // graph.display();
 
-        graph.addWieghtedGraph(edges, true);
-        graph.display();
+        // graph.addWieghtedGraph(edges, true);
+        // graph.display();
+
+        // graph.addEdgesInList(edges, false);
 
     }
 }
